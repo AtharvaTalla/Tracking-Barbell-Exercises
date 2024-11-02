32145b6461193de147889b70e6f7419ad2cd0953
Atharva Talla <148702004+AtharvaTalla@users.noreply.github.com>
2024-11-03 00:25:49 +0530

    update

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -12,27 +12,70 @@ from TemporalAbstraction import NumericalAbstraction
 df = pd.read_pickle("../../data/interim/02_outliers_removed_chauvanets.pkl")
 predictor_columns = list(df.columns[:6])
 
-# --------------------------------------------------------------
-# Dealing with missing values (imputation)
-# --------------------------------------------------------------
+# plot settings 
 
 plt.style.use("fivethirtyeight")
 plt.rcParams["figure.figsize"] = (20,5)
 plt.rcParams["figure.dpi"] = 100
 plt.rcParams["lines.linewidth"] = 2
 
+
 # --------------------------------------------------------------
-# Calculating set duration
+# Dealing with missing values (imputation)
 # --------------------------------------------------------------
 
 for col in predictor_columns:
     df[col] = df[col].interpolate()
 
 df.info()
+
+# --------------------------------------------------------------
+# Calculating set duration
+# --------------------------------------------------------------
+
+df[df["set"] == 25]["acc_y"].plot()
+df[df["set"] == 50]["acc_y"].plot()
+
+duration = df[df["set"] == 1].index[-1] - df[df["set"] == 1].index[0]
+duration.seconds    
+
+for s in df["set"].unique():
+    start = df[df["set"] == s].index[0]
+    stop = df[df["set"] == s].index[-1]
+
+    duration = start - stop
+    df.loc[(df["set"] == s), "duration"] = duration.seconds
+    
+duration_df = df.groupby(["category"])["duration"].mean()
+
+duration_df.iloc[0] / 5
+duration_df.iloc[1] / 10
+
 # --------------------------------------------------------------
 # Butterworth lowpass filter
 # --------------------------------------------------------------
 
+df_lowpass = df.copy()
+LowPass = LowPassFilter()
+
+fs = 1000 / 200
+cutoff = 1.2
+
+df_lowpass = LowPass.low_pass_filter(df_lowpass, "acc_y", fs, cutoff, order=5)
+
+subset = df_lowpass [df_lowpass ["set"] == 45]
+print (subset ["label"] [0])
+
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+ax[0].plot(subset ["acc_y"].reset_index(drop=True), label="raw data")
+ax[1].plot(subset ["acc_y_lowpass"]. reset_index(drop=True), label="butterworth filter")
+ax[0].legend (loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
+ax [1]. legend (loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True)
+
+for col in predictor_columns:
+    df_lowpass = LowPass.low_pass_filter(df_lowpass, col, fs, cutoff, order=5)
+    df_lowpass [col] = df_lowpass [col + "_lowpass"]
+    del df_lowpass [col + "_lowpass"]
 
 # --------------------------------------------------------------
 # Principal component analysis PCA
